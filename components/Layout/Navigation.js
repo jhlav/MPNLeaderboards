0d49d7d2833be7eb524024e9464f3818504d4252
jhlav <devjakob@gmail.com>
2017-01-21 17:04:47 -0500

    Update Header, add Leaderboards page.

--- components/Layout/Navigation.js
+++ components/Layout/Navigation.js
@@ -26,6 +26,7 @@ class Navigation extends React.Component {
       <nav className="mdl-navigation" ref={node => (this.root = node)}>
         <Link className="mdl-navigation__link" to="/">Home</Link>
         <Link className="mdl-navigation__link" to="/about">About</Link>
+        <Link className="mdl-navigation__link" to="/leaderboards">Leaderboards</Link>
       </nav>
     );
   }
---
+++ src/leaderboards/index.md
@@ -0,0 +1,5 @@
+---
+title: Leaderboards
+---
+
+Test
